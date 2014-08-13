1ef4237d7e2bb78c985f1ffbcdc877f3582a119a
Mercy Angela <mercyangelan@gmail.com>
2014-08-13 10:59:51 +0300

    7

--- 7.py
+++ 7.py
@@ -1,2 +1,13 @@
 """Define a function reverse() that computes the reversal of a string. For
 example, reverse("I am testing") should return the string "gnitset ma I"."""
+
+def reverse():
+    phrase = input("Enter the phrase you want to reverse: ")
+    new_phrase = ""
+    length = int(len(phrase))
+
+    for x in range(0, length):
+        new_phrase += phrase[-x-1]
+    print("The string in reverse is: ", new_phrase)
+
+reverse()
